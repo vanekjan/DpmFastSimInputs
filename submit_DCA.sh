0ac8b7df155eb27123877d8994a33dba2edc3520
Jan Vanek <17701469+vanekjan@users.noreply.github.com>
2023-06-27 13:22:16 -0400

    updated submit macros

---
+++ submit_DCA.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#copylist - Run16 Au+Au @ 200  GeV: startLine 1, maxLine 583172 in picoList_all_new.list
+#for VPDMB triggers start at line 4783
+#good HFT runs start at line 166378 (RunId > 17062047)
+#choose any prediefined number of lines from picoList_all_new.list
+sed -n '166378,200000 p' ./picoLists/picoList_all_new.list > picoList_submit.list
+
+#compile run macro locally and copy the compiled version (all files) in xml to scratch
+starver SL17d
+root -q -b -l compileRunMacroLocallyDCA.C
+
+path=`pwd -P`
+path=$( echo $path | sed 's|//|/|g' )
+
+echo executing submitPicoHFMakerDCA.csh f0r picoList_submit.list inside $path
+
+csh starSubmit/submitPicoHFMakerDCA.csh $path picoList_submit.list
---
+++ submit_DCA_sst_stream.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#copylist - Run16 Au+Au @ 200  GeV (production 2, sst+nosst stream): startLine 1, maxLine 125,618
+#choose any prediefined number of lines from picoList_all_new_sst_stream.list
+sed -n '100001,125618 p' ./picoLists/picoList_all_new_sst_stream.list > picoList_submit_sst.list
+
+#compile run macro locally and copy the compiled version (all files) in xml to scratch
+starver SL17d
+root -q -b -l compileRunMacroLocallyDCA.C
+
+path=`pwd -P`
+path=$( echo $path | sed 's|//|/|g' )
+
+
+echo executing submitPicoHFMakerDCA.csh f0r picoList_submit_sst.list inside $path
+
+csh starSubmit/submitPicoHFMakerDCA.csh $path picoList_submit_sst.list
